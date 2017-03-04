49e946baefa7bc050824c9124c78d5a6744c19f8
Brittany Torres <btorres@aclanyc.com>
2017-03-04 03:03:50 +0000

    thing

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 TinyTurtle.apply(window);
 
 // Type your directions to Tiny Turtle below
+forward(50);
+left(90);
+forward(50);
+left(90);
+forward(50);
+left(90);
+forward(50);
+left(90);
+forward(50);
+left(45);
+forward(35);
+left(90);
+forward(35);
+left(45);
+
+
 
 
 
 stamp();
+
